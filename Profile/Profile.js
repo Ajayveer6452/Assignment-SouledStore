2e1daa5a47209aaf334340090b5dc428ffcfab0c
AressMacMiniA1993 <aressmacminia1993@AressMacMiniA1993s-Mac-mini.local>
2019-08-09 14:38:17 +0530

    set data to list

--- Profile/Profile.js
+++ Profile/Profile.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import {
     StyleSheet,
     View,
-    Text
+    Text,
+    FlatList,TouchableOpacity,Image
 } from 'react-native';
+
+
 
 export default class Profile extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            invitedData:[  
+             { "Image": require('../src/t-shirt/t_shirt1.jpg'), "Description":"Max Men's Regular fit T-shirt", "Price":"₹600"},
+             { "Image": require('../src/t-shirt/t_shirt2.jpg'), "Description":"Max Men's Regular fit T-shirt", "Price":"₹600"},
+             { "Image": require('../src/t-shirt/t_shirt3.jpg'), "Description":"Max Men's Regular fit T-shirt", "Price":"₹600"},
+             { "Image": require('../src/t-shirt/t_shirt4.jpg'), "Description":"Max Men's Regular fit T-shirt", "Price":"₹600"},
+             { "Image": require('../src/t-shirt/t_shirt1.jpg'), "Description":"Max Men's Regular fit T-shirt", "Price":"₹600"},
+             { "Image": require('../src/t-shirt/t_shirt3.jpg'), "Description":"Max Men's Regular fit T-shirt", "Price":"₹600"},
+             { "Image": require('../src/t-shirt/t_shirt2.jpg'), "Description":"Max Men's Regular fit T-shirt", "Price":"₹600"},
+             { "Image": require('../src/t-shirt/t_shirt4.jpg'), "Description":"Max Men's Regular fit T-shirt", "Price":"₹600"},
+             { "Image": require('../src/t-shirt/t_shirt1.jpg'), "Description":"Max Men's Regular fit T-shirt", "Price":"₹600"},
 
+            ]  
         };
     }
 
@@ -26,7 +40,48 @@ export default class Profile extends Component {
 
             <View style={styles.container} >
 
-                <Text style={{ margin: 10, fontSize: 18 }}> Profile Comming Soon..</Text>
+                <FlatList
+                style={{  width: '100%',
+                height: '100%' }}
+                data={this.state.invitedData}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item, index }) => {
+
+                    return (
+                        <TouchableOpacity
+                            style={styles.listItem}
+                            activeOpacity={.5} onPress={() => {
+
+                                // Actions.TaskDetails({ ShiftData: item, refreshingShift: this.GetTaskData.bind(this) })
+
+
+                            }}    >
+
+                            <View style={styles.listCard}>
+
+                            <Image source={item.Image}   style={styles.profileImg}/>
+                           
+                                    <View style={{ flex: 1, flexDirection: 'row', }}>
+
+                                            <Text style={{ color: '#4E4E4E',fontWeight: 'bold', fontSize:16 }}>{item.Description} </Text>
+                                   
+
+                                    </View>
+
+                                    <View style={{ flex: 1, flexDirection: 'row', }}>
+
+                                            <Text numberOfLines={0} style={{ color: '#4E4E4E',fontWeight: 'bold', fontSize:16}}>{item.Price} </Text>
+                        
+                                    </View>
+
+                            </View>
+
+                        </TouchableOpacity>
+
+                    );
+                }}
+            >
+            </FlatList>
 
             </View>
            
@@ -36,6 +91,29 @@ export default class Profile extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        padding:20
     },
+    listCard: {
+        flex: 1,
+        backgroundColor: 'white',
+        borderBottomWidth: 3,
+        borderTopWidth: 1,
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        alignItems: 'center',
+        borderColor: 'rgb(201, 207, 216)',
+        borderRadius: 6,
+    },
+    listItem: {
+        marginBottom: 10,
+    },
+    profileImg: {
+        marginTop:5,
+        height: 300,
+        width: 200,
+        borderRadius: 40,
+        alignSelf:'center'
+      },
+
 })
